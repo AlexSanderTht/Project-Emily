dd08872277ca8e3ee8cdc41181fb80d2d6235ff9
Alex Sander Perini Dos Santos <alex.santos@a1.com.br>
2026-04-06 08:50:04 -0300

    [REFATORAÇÃO] Ajustando arquivos inúteis.

--- kill_cad.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-echo Fechando processos do CAD...
-taskkill /F /T /IM acad.exe >nul 2>&1
-exit
--- task.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-start  "celery_default" cmd /k "cd /d C:\EIA1Project\venv\Scripts && activate && cd /d C:\EIA1Project\htdocs\eia1ferramentas\weba1\ && celery -A weba1 purge -Q default --force && celery -A weba1 worker -Q default -l info -n worker_default@%h --concurrency 50 -P threads "
-start "celery_files_dwg" cmd /k "cd /d C:\EIA1Project\venv\Scripts && activate && cd /d C:\EIA1Project\htdocs\eia1ferramentas\weba1\ && celery -A weba1 purge -Q files_using_dwg --force && celery -A weba1 worker -Q files_using_dwg -l info -n worker_files_dwg@%h --concurrency 1 -P solo "
-
